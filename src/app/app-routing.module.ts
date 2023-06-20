46cdd4808e310dde857931cd2fbc23cb3ea40ec2
Nycollas <sobolevski.academy@gmail.com>
2023-06-20 11:47:33 -0300

    UPDATE: style login & signIn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", title:"Discordia - Home", component: HomePageComponent},
+  {path: "login-page", title:"Discordia - Login", component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { NewAccountPageComponent } from './new-account-page/new-account-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginComponent } from './login/login.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    NewAccountPageComponent,
     LoginPageComponent,
     LoginComponent,
-    NewAccountComponent
+    NewAccountComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
